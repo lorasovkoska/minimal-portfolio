14a9d955a1127b5962d0bdf405bab28f7b1122da
Lora Sovkoska <lora.sovkoska@iris-worldwide.com>
2025-11-18 19:22:37 +0100

    deployed studio setup

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
-import { schemaTypes } from './schema'
+import { schemaTypes } from './sanity/schema'
 
 export default defineConfig({
   name: 'minimal-portfolio',
   title: 'Minimal Portfolio',
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'placeholder',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'wwl2cg0i',
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   basePath: '/studio',
   plugins: [structureTool()],
